38a5fd38837b00f0fd247876ebf9ff71b13d23dd
GenesisNUP30 <jdirproy@gmail.com>
2026-03-22 00:27:50 +0100

    Redireccionar a dashboard al hacer login

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { loginRequest } from "../services/authServices";
 import { useAuthStore } from "../store/authStore";
+import { ROUTES } from "../../../routes/routes";
 
 export default function useLogin() {
     const  setAuth = useAuthStore((state) => state.setAuth);
+    const navigate = useNavigate();
 
     const login = async (data: any) => {
         const response = await loginRequest(data);
         setAuth(response.user, response.token, data.remember);
+        navigate(ROUTES.DASHBOARD);
         return response;
     };
 
