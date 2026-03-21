b1cce2dfc112a77c07a892187d1c92c9c7309803
GenesisNUP30 <jdirproy@gmail.com>
2026-03-21 20:16:15 +0100

    Logica de inicio de sesion y actualizacion del estado del usuario, token y recordar usuario

---
+++ src/features/auth/hooks/useLogin.ts
@@ -0,0 +1,14 @@
+import { loginRequest } from "../services/authServices";
+import { useAuthStore } from "../store/authStore";
+
+export default function useLogin() {
+    const  setAuth = useAuthStore((state) => state.setAuth);
+
+    const login = async (data: any) => {
+        const response = await loginRequest(data);
+        setAuth(response.user, response.token, data.remember);
+        return response;
+    };
+
+    return login;
+}
