8bf7c2b85422731d9898dd8918d4fc8ba8f4bf9a
GenesisNUP30 <jdirproy@gmail.com>
2026-03-21 20:12:11 +0100

    Logica para guardar el usuaio y token con persistencia de recordar credenciales

---
+++ src/features/auth/store/authStore.ts
@@ -0,0 +1,57 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { STORAGE_KEYS } from "../../../constants/storageKeys";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  rol: string;
+}
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  isHydrated: boolean;
+  setAuth: (user: User, token: string, remember: boolean) => void;
+  logout: () => void;
+  setHydrated: () => void;
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isHydrated: false,
+
+      setAuth: (user, token, remember) => {
+        if (remember) {
+          localStorage.setItem(STORAGE_KEYS.TOKEN, token);
+          localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(user));
+          localStorage.setItem(STORAGE_KEYS.REMEMBER, "true");
+        } else {
+          localStorage.removeItem(STORAGE_KEYS.REMEMBER);
+        }
+
+        set({ user, token });
+      },
+
+      logout: () => {
+        localStorage.removeItem(STORAGE_KEYS.TOKEN);
+        localStorage.removeItem(STORAGE_KEYS.USER);
+        localStorage.removeItem(STORAGE_KEYS.REMEMBER);
+
+        set({ user: null, token: null });
+      },
+
+      setHydrated: () => set({ isHydrated: true }),
+    }),
+    {
+      name: "auth-storage", // Clave explícita para LocalStorage
+      onRehydrateStorage: () => (state) => {
+        state?.setHydrated();
+      },
+    },
+  ),
+);
