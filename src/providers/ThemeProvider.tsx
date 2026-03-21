9b4c14750888604a62a73b5bbd838a0492e14d8e
GenesisNUP30 <jdirproy@gmail.com>
2026-03-21 22:11:38 +0100

    Configurar tema global de app

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { theme } from '../theme/theme';
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
+}
