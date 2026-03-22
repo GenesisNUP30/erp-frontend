bdd2473e51058146588db5c2a3f1117615d60cbe
GenesisNUP30 <jdirproy@gmail.com>
2026-03-23 00:13:25 +0100

    Configurar el entorno para subir el front

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './routes/AppRouter';
-import ThemeProvider from './providers/ThemeProvider';
-
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./routes/AppRouter";
+import ThemeProvider from "./providers/ThemeProvider";
 
 function App() {
-  
+  const baseName = import.meta.env.PROD
+    ? "/proyectos/2026/genesisnatalya/Proyecto_TFG/frontend"
+    : "/";
 
   return (
     <>
       <ThemeProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={baseName}>
           <AppRouter />
         </BrowserRouter>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' 
+      ? '/proyectos/2026/genesisnatalya/Proyecto_TFG/frontend/' 
+      : '/',
+  }
 })
