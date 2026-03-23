c3d3b9d10b6f6847d4497e62dff937212b9327c9
GenesisNUP30 <jdirproy@gmail.com>
2026-03-23 03:33:13 +0100

    Custom hook para cargar datos, gestionar estado y filtrar en tiempo real

--- src/features/workers/services/workersServices.ts
+++ src/features/workers/services/workersServices.ts
@@ -1,6 +1,16 @@
 import apiClient from '../../../api/apiClient';
+import type { Worker } from '../types/IWorkers';
 
-export const getWorkersRequest = async () => {
-  const response = await apiClient.get('/trabajadores');
-  return response.data;
+type ApiResponse<T> = {
+  success: boolean;
+  data: T;
+  count?: number;
+};
+
+export const getWorkersRequest = async (): Promise<Worker[]> => {
+  const response = await apiClient.get<ApiResponse<Worker[]>>(
+    '/trabajadores',
+  );
+
+  return response.data.data;
 };
