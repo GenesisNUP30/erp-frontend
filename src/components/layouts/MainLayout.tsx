b730c738a4c86d9ec733fa70110994afdf08fc25
GenesisNUP30 <jdirproy@gmail.com>
2026-03-23 02:37:21 +0100

    Sidebar incial creado

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,20 +1,18 @@
-import { Box } from '@mui/material';
-import { useState } from 'react';
-import Sidebar from '../shared/Sidebar';
+import { Box } from "@mui/material";
+import { useState } from "react";
+import Sidebar from "../shared/Sidebar";
+import { Outlet } from "react-router-dom";
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [open, setOpen] = useState(true);
+export default function MainLayout() {
+  //const [open, setOpen] = useState(true);
+  const [open] = useState(true);
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <Sidebar open={open} />
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {children}
+        <Outlet />
       </Box>
     </Box>
   );
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,21 +1,25 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import DashboardPage from '../pages/DashboardPage';
-import ProtectedRoute from './ProtectedRoute';
-import { ROUTES } from './routes';
-import LoginPage from '../features/auth/pages/LoginPage';
+import { Routes, Route, Navigate } from "react-router-dom";
+import DashboardPage from "../pages/DashboardPage";
+import ProtectedRoute from "./ProtectedRoute";
+import { ROUTES } from "./routes";
+import LoginPage from "../features/auth/pages/LoginPage";
+import MainLayout from "../components/layouts/MainLayout";
+
 
 export default function AppRouter() {
   return (
     <Routes>
       {/* Redirección inicial */}
-      <Route path='/' element={<Navigate to={ROUTES.LOGIN} />} />
+      <Route path="/" element={<Navigate to={ROUTES.LOGIN} />} />
 
       {/* Públicas */}
       <Route path={ROUTES.LOGIN} element={<LoginPage />} />
 
       {/* Protegidas */}
       <Route element={<ProtectedRoute />}>
-        <Route path={ROUTES.DASHBOARD} element={<DashboardPage />} />
+        <Route element={<MainLayout />}>
+          <Route path={ROUTES.DASHBOARD} element={<DashboardPage />} />
+        </Route>
       </Route>
     </Routes>
   );
