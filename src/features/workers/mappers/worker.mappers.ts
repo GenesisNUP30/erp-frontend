b911537b0168e212c9d7da6a9fdf458f183bc193
GenesisNUP30 <jdirproy@gmail.com>
2026-03-24 21:58:32 +0100

    Actualizar hook usando mappers

---
+++ src/features/workers/mappers/worker.mappers.ts
@@ -0,0 +1,19 @@
+import type { Role } from '../../../constants/roles';
+import type { WorkerFormData } from '../schema/workerSchema';
+import type { CreateWorkerDTO } from '../types/IWorkers';
+
+/**
+ * Mapea datos del formulario → DTO del backend
+ */
+export const mapWorkerFormToCreateDTO = (
+  data: WorkerFormData,
+): CreateWorkerDTO => ({
+  name: data.name,
+  username: data.username,
+  email: data.email,
+  password: data.password || '',
+  dni: data.dni,
+  telefono: data.telefono,
+  rol: data.rol as Role,
+  fecha_alta: data.fecha_alta,
+});
