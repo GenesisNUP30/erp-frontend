8246f401afe65f4596583dd6771fd369de576b1a
GenesisNUP30 <jdirproy@gmail.com>
2026-04-04 23:43:25 +0200

    Dirigir a la pagina de detalle al pulsar Ver detalle

--- src/features/workers/pages/WorkersPage.tsx
+++ src/features/workers/pages/WorkersPage.tsx
@@ -1,16 +1,32 @@
 import { Box, Typography, CircularProgress } from "@mui/material";
 import useWorkers from "../hooks/useWorkers";
+import { useNavigate } from "react-router-dom"; 
+import { ROUTES } from "../../../routes/routes";
 import WorkersFilters from "../components/list/WorkersFilters";
 import WorkersTable from "../components/list/WorkersTable";
 import CreateWorker from "../components/create/CreateWorker";
 import { useState } from "react";
+import type { Worker } from "../types/IWorkers";
 
 export default function WorkersPage() {
+  const navigate = useNavigate();
   const { workers, search, setSearch, loading, refresh } = useWorkers();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
+
+  // Lógica para ir al detalle 
+  const handleViewDetails = (worker: Worker) => {
+    const path = ROUTES.WORKER_DETAILS.replace(':id', worker.id.toString());
+    navigate(path);
+  };
+
+  // Lógica para ir a editar 
+  const handleEditWorker = (worker: Worker) => {
+    // Por ahora lo dejamos apuntando al detalle o donde prefieras
+    console.log("Editando trabajador:", worker.id);
+  };
 
   const handleSuccess = () => {
     handleCloseModal();
@@ -37,7 +53,12 @@ export default function WorkersPage() {
           <CircularProgress />
         </Box>
       ) : (
-        <WorkersTable workers={workers} loading={loading} onViewDetails={handleOpenModal} onEditWorker={handleOpenModal} />
+        <WorkersTable 
+        workers={workers} 
+        loading={loading} 
+        onViewDetails={handleViewDetails} 
+        onEditWorker={handleEditWorker} 
+        />
       )}
 
       {/* MODAL DE CREACIÓN */}
