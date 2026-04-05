b9e46fb3cd6e6762204df84586ee58d71ef33260
GenesisNUP30 <jdirproy@gmail.com>
2026-04-06 01:21:52 +0200

    Usar controlador para mostrar el valor del Rol

--- src/features/workers/components/create/WorkerFields.tsx
+++ src/features/workers/components/create/WorkerFields.tsx
@@ -12,7 +12,7 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Controller } from "react-hook-form";
-import type { Control,UseFormRegister, FieldErrors } from "react-hook-form";
+import type { Control, UseFormRegister, FieldErrors } from "react-hook-form";
 import type { WorkerFormData } from "../../schema/workerSchema";
 import v from "../../../../validations/validations";
 import dayjs from "dayjs";
@@ -24,14 +24,19 @@ interface Props {
   serverErrors?: Record<string, string[]> | null;
 }
 
-export default function WorkerFields({ register, errors, control, serverErrors }: Props) {
-
-  // Función auxiliar para obtener el mensaje de error 
+export default function WorkerFields({
+  register,
+  errors,
+  control,
+  serverErrors,
+}: Props) {
+  // Función auxiliar para obtener el mensaje de error
   const getError = (fieldName: keyof WorkerFormData) => {
-    // Error de Zod 
+    // Error de Zod
     if (errors[fieldName]) return errors[fieldName]?.message;
     // Error de Back
-    if (serverErrors && serverErrors[fieldName]) return serverErrors[fieldName][0];
+    if (serverErrors && serverErrors[fieldName])
+      return serverErrors[fieldName][0];
     return null;
   };
 
@@ -44,65 +49,109 @@ export default function WorkerFields({ register, errors, control, serverErrors }
 
         {/* Fila 1: Nombre y Usuario */}
         <Box display="flex" gap={2} flexWrap="wrap">
-          <FormControl error={!!getError('name')} sx={{ flex: '1 1 300px' }}>
-            <InputLabel htmlFor="name">{v.entities.workers.labels.name}</InputLabel>
-            <OutlinedInput id="name" label={v.entities.workers.labels.name} {...register('name')} />
-            <FormHelperText>{getError('name')}</FormHelperText>
+          <FormControl error={!!getError("name")} sx={{ flex: "1 1 300px" }}>
+            <InputLabel htmlFor="name">
+              {v.entities.workers.labels.name}
+            </InputLabel>
+            <OutlinedInput
+              id="name"
+              label={v.entities.workers.labels.name}
+              {...register("name")}
+            />
+            <FormHelperText>{getError("name")}</FormHelperText>
           </FormControl>
 
-          <FormControl error={!!getError('username')} sx={{ flex: '1 1 200px' }}>
-            <InputLabel htmlFor="username">{v.entities.workers.labels.username}</InputLabel>
-            <OutlinedInput id="username" label={v.entities.workers.labels.username} {...register('username')} />
-            <FormHelperText>{getError('username')}</FormHelperText>
+          <FormControl
+            error={!!getError("username")}
+            sx={{ flex: "1 1 200px" }}
+          >
+            <InputLabel htmlFor="username">
+              {v.entities.workers.labels.username}
+            </InputLabel>
+            <OutlinedInput
+              id="username"
+              label={v.entities.workers.labels.username}
+              {...register("username")}
+            />
+            <FormHelperText>{getError("username")}</FormHelperText>
           </FormControl>
         </Box>
 
         {/* Fila 2: DNI y Rol */}
         <Box display="flex" gap={2} flexWrap="wrap">
-          <FormControl error={!!getError('dni')} sx={{ flex: "1 1 200px" }}>
-            <InputLabel htmlFor="dni">{v.entities.workers.labels.dni}</InputLabel>
-            <OutlinedInput id="dni" label={v.entities.workers.labels.dni} {...register('dni')} />
-            <FormHelperText>{getError('dni')}</FormHelperText>
+          <FormControl error={!!getError("dni")} sx={{ flex: "1 1 200px" }}>
+            <InputLabel htmlFor="dni">
+              {v.entities.workers.labels.dni}
+            </InputLabel>
+            <OutlinedInput
+              id="dni"
+              label={v.entities.workers.labels.dni}
+              {...register("dni")}
+            />
+            <FormHelperText>{getError("dni")}</FormHelperText>
           </FormControl>
 
-          <FormControl error={!!getError('rol')} sx={{ flex: '1 1 200px' }}>
-            <InputLabel id='rol-label'>{v.entities.workers.labels.rol}</InputLabel>
-            <Select labelId="rol-label" id="rol" defaultValue='' label={v.entities.workers.labels.rol} {...register('rol')}>
-              <MenuItem value="recolector">Recolector</MenuItem>
-              <MenuItem value="encargado">Encargado</MenuItem>
-              <MenuItem value="administrador">Administrador</MenuItem>
-            </Select>
-            <FormHelperText>{getError('rol')}</FormHelperText>
+          <FormControl error={!!getError("rol")} sx={{ flex: "1 1 200px" }}>
+            <InputLabel id="rol-label">
+              {v.entities.workers.labels.rol}
+            </InputLabel>
+            <Controller
+              name="rol"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field} 
+                  labelId="rol-label"
+                  id="rol"
+                  label={v.entities.workers.labels.rol}
+                >
+                  <MenuItem value="recolector">Recolector</MenuItem>
+                  <MenuItem value="encargado">Encargado</MenuItem>
+                  <MenuItem value="administrador">Administrador</MenuItem>
+                </Select>
+              )}
+            />
+            <FormHelperText>{getError("rol")}</FormHelperText>
           </FormControl>
         </Box>
 
         {/* Fila 3: Email y Teléfono */}
         <Box display="flex" gap={2} flexWrap="wrap">
-          <FormControl error={!!getError('email')} sx={{ flex: "1 1 300px" }}>
-            <InputLabel htmlFor="email">{v.entities.workers.labels.email}</InputLabel>
+          <FormControl error={!!getError("email")} sx={{ flex: "1 1 300px" }}>
+            <InputLabel htmlFor="email">
+              {v.entities.workers.labels.email}
+            </InputLabel>
             <OutlinedInput
               id="email"
               type="email"
               label={v.entities.workers.labels.email}
               {...register("email")}
             />
-            <FormHelperText>{getError('email')}</FormHelperText>
+            <FormHelperText>{getError("email")}</FormHelperText>
           </FormControl>
 
-          <FormControl error={!!getError('telefono')} sx={{ flex: "1 1 200px" }}>
-            <InputLabel htmlFor="telefono">{v.entities.workers.labels.telefono}</InputLabel>
+          <FormControl
+            error={!!getError("telefono")}
+            sx={{ flex: "1 1 200px" }}
+          >
+            <InputLabel htmlFor="telefono">
+              {v.entities.workers.labels.telefono}
+            </InputLabel>
             <OutlinedInput
               id="telefono"
               label={v.entities.workers.labels.telefono}
               {...register("telefono")}
             />
-            <FormHelperText>{getError('telefono')}</FormHelperText>
+            <FormHelperText>{getError("telefono")}</FormHelperText>
           </FormControl>
         </Box>
 
         {/* Fila 4: Fecha Alta y Password (si fuera necesario) */}
         <Box display="flex" gap={2} flexWrap="wrap">
-          <FormControl error={!!getError('fecha_alta')} sx={{ flex: "1 1 200px" }}>
+          <FormControl
+            error={!!getError("fecha_alta")}
+            sx={{ flex: "1 1 200px" }}
+          >
             <Controller
               name="fecha_alta"
               control={control}
@@ -115,19 +164,29 @@ export default function WorkerFields({ register, errors, control, serverErrors }
                   }}
                   slotProps={{
                     textField: {
-                      error: !!getError('fecha_alta'),
+                      error: !!getError("fecha_alta"),
                     },
                   }}
                 />
               )}
             />
-            <FormHelperText>{getError('fecha_alta')}</FormHelperText>
+            <FormHelperText>{getError("fecha_alta")}</FormHelperText>
           </FormControl>
 
-          <FormControl error={!!getError('password')} sx={{ flex: '1 1 200px' }}>
-            <InputLabel htmlFor="password">{v.entities.workers.labels.password}</InputLabel>
-            <OutlinedInput id="password" type="password" label={v.entities.workers.labels.password} {...register('password')} />
-            <FormHelperText>{getError('password')}</FormHelperText>
+          <FormControl
+            error={!!getError("password")}
+            sx={{ flex: "1 1 200px" }}
+          >
+            <InputLabel htmlFor="password">
+              {v.entities.workers.labels.password}
+            </InputLabel>
+            <OutlinedInput
+              id="password"
+              type="password"
+              label={v.entities.workers.labels.password}
+              {...register("password")}
+            />
+            <FormHelperText>{getError("password")}</FormHelperText>
           </FormControl>
         </Box>
       </Box>
