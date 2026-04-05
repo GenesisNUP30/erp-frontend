6a1c38a71107a0de1e51ae1bb2fcd3c17543f0ef
GenesisNUP30 <jdirproy@gmail.com>
2026-04-05 23:24:21 +0200

    Agregar ruta de edicion

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,8 @@
 export const ROUTES = {
   ROOT: '/',
-  LOGIN: '/login',
-  DASHBOARD: '/dashboard',
-  WORKERS: '/trabajadores',
+  LOGIN: '/login', // Ruta para el login
+  DASHBOARD: '/dashboard', // Ruta para el dashboard
+  WORKERS: '/trabajadores', // Ruta para la lista
   WORKER_DETAILS: '/trabajadores/detalles/:id', // Ruta para el detalle
+  WORKER_EDIT: '/trabajadores/editar/:id', // Ruta para el editar
 } as const;
