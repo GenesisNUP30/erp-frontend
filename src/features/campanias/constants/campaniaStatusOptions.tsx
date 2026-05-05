5c6f1fa92f4dbe634e4c3c439d5999c4bf4c8605
GenesisNUP30 <jdirproy@gmail.com>
2026-05-05 12:49:15 +0200

    Constante para el estado

---
+++ src/features/campanias/constants/campaniaStatusOptions.tsx
@@ -0,0 +1,26 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import FlagIcon from '@mui/icons-material/Flag';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import type { StatusOption } from '../../../components/shared/StatusChip';
+import v from '../../../validations/validations';
+
+export const campaniaStatusOptions: StatusOption[] = [
+  {
+    label: v.entities.campanias.status.activa,
+    value: 'activa',
+    color: 'success',
+    icon: <CheckCircleIcon sx={{ fontSize: '16px' }} />,
+  },
+  {
+    label: v.entities.campanias.status.finalizada,
+    value: 'finalizada',
+    color: 'default',
+    icon: <FlagIcon sx={{ fontSize: '16px' }} />,
+  },
+  {
+    label: v.entities.campanias.status.planificada,
+    value: 'planificada',
+    color: 'info',
+    icon: <ScheduleIcon sx={{ fontSize: '16px' }} />,
+  },
+];
